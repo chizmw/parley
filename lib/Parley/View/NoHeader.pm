a180c7282be59373de967562e71d4925313924ad
chisel <chisel@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-01-23 23:08:24 +0000

    r12583@ferrari:  chisel | 2006-01-23 19:29:52 +0000  + new TT view that *doesn't* auto-include header and footer files - used by    some AJAX functions for returning html blocks, etc

---
+++ lib/Parley/View/NoHeader.pm
@@ -0,0 +1,29 @@
+package Parley::View::NoHeader;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+=head1 NAME
+
+Parley::View::NoHeader - Catalyst TT View
+
+=head1 SYNOPSIS
+
+See L<Parley>
+
+=head1 DESCRIPTION
+
+Catalyst TT View.
+
+=head1 AUTHOR
+
+Chisel Wright,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
