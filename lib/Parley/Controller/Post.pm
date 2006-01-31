c3c7def44347da6b574e3618d36b5284b4fbc7e9
chisel <chisel@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-01-31 11:10:22 +0000

    r12676@ferrari:  chisel | 2006-01-30 08:53:06 +0000  + update list of Changes  - remove helper generated blocks and fix up AUTHOR in Post.pm  + add functionality to POst.pm to allow users to edit their own posts  / modified some template behaviour and appearance

--- lib/Parley/Controller/Post.pm
+++ lib/Parley/Controller/Post.pm
@@ -3,6 +3,7 @@ package Parley::Controller::Post;
 use strict;
 use warnings;
 use base 'Catalyst::Controller';
+use List::MoreUtils qw{ uniq };
 
 =head1 NAME
 
@@ -20,24 +21,9 @@ Catalyst Controller.
 
 =cut
 
-#
-# Uncomment and modify this or add new actions to fit your needs
-#
-#=head2 default
-#
-#=cut
-#
-#sub default : Private {
-#    my ( $self, $c ) = @_;
-#
-#    # Hello World
-#    $c->response->body('Parley::Controller::Post is on Catalyst!');
-#}
-
-
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< <pause@herlpacker.co.uk> >>
 
 =head1 LICENSE
 
@@ -45,5 +31,80 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+use Data::FormValidator 4.02;
+our $DFV;
+
+BEGIN {
+    $DFV = Data::FormValidator->new(
+        {
+            edit_post => {
+                required        => [qw/post_message/],
+                field_filters   => {
+                    post_message    => 'trim',
+                },
+                msgs => {
+                    missing => q{One or more required fields are missing},
+                    format  => '%s',
+                },
+            },
+        }
+    )
+}
+
+sub edit : Local {
+    my ($self, $c) = @_;
+    my (@messages);
+
+    # need to be logged in to edit post
+    Parley::App::Helper->login_if_required($c, q{You must be logged in to edit your posts});
+
+    # you can only edit posts you've created
+    if ($c->session->{authed_user}->id() != $c->stash->{current_post}->creator()->id()) {
+        push @messages, q{You can only edit your own posts};
+    }
+
+    # you can't edit a locked post (not that you can see the link unless you're hacking)
+    elsif ($c->stash->{current_post}->thread()->locked()) {
+        push @messages, q{You can't edit locked posts};
+    }
+
+    # deal with any actual form submits
+    elsif ($c->request->param('post_update')) {
+        @messages = $self->_edit_post( $c );
+    }
+
+    if (scalar @messages) {
+        @messages = uniq(sort(@messages));
+        $c->stash->{messages} = \@messages;
+    }
+}
+
+sub _edit_post {
+    my ($self, $c) = @_;
+    my ($results, @messages);
+
+    if ($DFV) {
+        $results = $DFV->check($c->req->parameters, 'edit_post');
+    };
+
+    if ($results || !$DFV) {
+        # update the post with the new information
+        $c->stash->{current_post}->message( $results->valid->{post_message} );
+
+        $c->stash->{current_post}->update();
+
+        # view the thread - but only if no errors
+        if (not scalar(@messages)) {
+            $c->response->redirect( $c->req->base . 'thread/view?thread=' . $c->stash->{current_post}->thread()->id() );
+        }
+    }
+    else {
+        $c->log->error('DFV failed');
+        push @messages, map {$_} values %{$results->msgs};
+    }
+
+    return (uniq(sort @messages));
+}
 
 1;
