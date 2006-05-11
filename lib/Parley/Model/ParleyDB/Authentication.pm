e0bdfcaaf5716f77163124e581a5556f49ede64a
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-05-11 19:51:44 +0000

    r14413@ferrari:  chisel | 2006-05-11 09:03:36 +0100  After upgrading to DBIC 5.x I starting having problems (with only table  'authentication') with ->create() not returning the PK field in the new object  To try to rectify this I've specified the details for this table - it made no difference.
     / alter AUTHOR email address

--- lib/Parley/Model/ParleyDB/Authentication.pm
+++ lib/Parley/Model/ParleyDB/Authentication.pm
@@ -4,6 +4,17 @@ use strict;
 use warnings;
 use base 'DBIx::Class::Core';
 
+__PACKAGE__->table('authentication');
+__PACKAGE__->add_columns(qw/
+    authentication_id
+    username
+    password
+    authenticated
+/);
+__PACKAGE__->set_primary_key('authentication_id');
+__PACKAGE__->sequence('authentication_authentication_id_seq');
+
+
 =head1 NAME
 
 Parley::Model::ParleyDB::Authentication - Catalyst DBIC Table Model
@@ -18,7 +29,7 @@ Catalyst DBIC Table Model.
 
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< <pause@herlpacker.co.uk> >>
 
 =head1 LICENSE
 
