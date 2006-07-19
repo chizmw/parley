e729977ecb44bd7ef9048cb1e6f4926e52e121d0
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-07-19 16:21:35 +0000

    r4890@ferrari:  chisel | 2006-07-19 09:55:51 +0100  + added class methods: watching_thread(), new_post_alert_list()

--- lib/Parley/Model/ParleyDB/Thread.pm
+++ lib/Parley/Model/ParleyDB/Thread.pm
@@ -3,6 +3,8 @@ package Parley::Model::ParleyDB::Thread;
 use strict;
 use warnings;
 use base 'DBIx::Class::Core';
+use Carp;
+use DateTime::Format::Pg;
 
 __PACKAGE__->belongs_to(
     last_post => 'Parley::Model::ParleyDB::Post',
@@ -24,6 +26,65 @@ __PACKAGE__->inflate_column(
     }
 );
 
+sub watching_thread {
+    my ($self, $thread, $person) = @_;
+
+    if (not defined $thread) {
+        warn 'undefined value passed as $thread in watching_thread()';
+        return;
+    }
+    if (not defined $person) {
+        warn 'undefined value passed as $person in watching_thread()';
+        return;
+    }
+
+    my $thread_view = Parley::Model::ParleyDB::ThreadView->find(
+        {
+            person  => $person->id(),
+            thread  => $thread->id(),
+        }
+    );
+
+    return $thread_view->watched();
+}
+
+sub new_post_alert_list {
+    my ($self, $thread, $last_post) = @_;
+
+    if (not defined $thread) {
+        Carp::carp('undefined value passed as $thread in new_post_alert_list()');
+        return;
+    }
+    if (not defined $last_post) {
+        Carp::carp('undefined value passed as $last_post in new_post_alert_list()');
+        return;
+    }
+
+    # - get a list of people watching thread X
+    # - whos last_viewed timestamp is LESS than the timestamp of the last post in the thread
+    #
+    # This would give an alert for each new post ... so:
+    #
+    # - only email matches where last_viewed timestamp > last_notified (or is NULL)
+    my $list = Parley::Model::ParleyDB::ThreadView->search(
+        {
+            watched     => 1,
+            thread      => $thread->id(),
+            person      => { '!=', $last_post->creator->id() },
+            timestamp   => { '<', DateTime::Format::Pg->format_datetime($last_post->created()) },
+             
+            last_notified   => [
+                -and =>
+                #{ '<', DateTime::Format::Pg->format_datetime($last_post->created()) },
+                    [ \'< timestamp', {'==', undef} ],
+            ],
+
+        },
+    );
+
+    return $list;
+}
+
 =head1 NAME
 
 Parley::Model::ParleyDB::Thread - Catalyst DBIC Table Model
@@ -38,7 +99,7 @@ Catalyst DBIC Table Model.
 
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< <chisel@herlpacker.co.uk> >>
 
 =head1 LICENSE
 
