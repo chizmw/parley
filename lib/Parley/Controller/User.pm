60e7c9ecb37f14f64501854c547dbc2f1f4d55f0
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-08-16 13:44:17 +0000

    r5143@ferrari:  chisel | 2006-08-14 18:58:32 +0100  + add /user/login Path action

--- lib/Parley/Controller/User.pm
+++ lib/Parley/Controller/User.pm
@@ -4,6 +4,64 @@ use strict;
 use warnings;
 use base 'Catalyst::Controller';
 
+# deal with user login requests on user/login
+sub login : Path('/user/login') {
+    my ( $self, $c ) = @_;
+
+    # default form message
+    $c->stash->{'message'} = 'Please enter your username and password';
+    # if we have a custom message to use ..
+    $c->stash->{'login_message'} = delete( $c->session->{login_message} );
+
+    # if we have a username, try to log the user in
+    if ( $c->request->param('username') ) {
+        # try to log the user in
+        my $login_status = $c->login(
+            $c->request->param('username'),
+            $c->request->param('password'),
+        );
+
+        # if we have a user we're logged in
+        if ( $login_status ) {
+            my $base    = $c->request->base();
+            my $action  = $c->request->action();
+
+            $c->log->debug("base:   $base");
+            $c->log->debug("action: $action");
+
+            # if we've stored somewhere to go after we log-in, got there now
+            if ( $c->session->{after_login} ) {
+                $c->response->redirect( delete $c->session->{after_login} );
+            }
+
+            # (else) if we've just been through Forgotten Password, don't go back there
+            elsif ($c->request->referer() =~ m{user/password/}) {
+                # go to the default app URL
+                $c->response->redirect( $c->uri_for($c->config()->{default_uri}) );
+            }
+
+            # (else) redirect to where we were referred from, unless our referer is our action
+            elsif ( $c->request->referer() =~ m{\A$base}xms and $c->request->referer() !~ m{$action\z}xms) {
+                # go to where we came from
+                $c->response->redirect( $c->request->referer() );
+            }
+
+            # (else) if all else fails go to the application's default_uri
+            else {
+                $c->response->redirect( $c->uri_for($c->config()->{default_uri}) );
+            }
+        }
+
+        # otherwise we failed to login, try again!
+        else {
+            $c->stash->{'message'}
+                = 'Unable to authenticate the login details supplied';
+        }
+    }
+}
+
+
+
 1;
 
 __END__
