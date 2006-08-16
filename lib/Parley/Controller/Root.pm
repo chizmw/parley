41ee91e4bcc4c5f96bbd3a5a7684a4f8663a89b1
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-08-16 13:41:19 +0000

    r5112@ferrari:  chisel | 2006-08-10 09:04:53 +0100  + run: catalyst.pl Parley

---
+++ lib/Parley/Controller/Root.pm
@@ -0,0 +1,55 @@
+package Parley::Controller::Root;
+
+use strict;
+use warnings;
+use base 'Catalyst::Controller';
+
+#
+# Sets the actions in this controller to be registered with no prefix
+# so they function identically to actions created in MyApp.pm
+#
+__PACKAGE__->config->{namespace} = '';
+
+=head1 NAME
+
+Parley::Controller::Root - Root Controller for Parley
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=cut
+
+=head2 default
+
+=cut
+
+sub default : Private {
+    my ( $self, $c ) = @_;
+
+    # Hello World
+    $c->response->body( $c->welcome_message );
+}
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut 
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+Chisel Wright,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
