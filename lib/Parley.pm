77b39f522489f274add28169e0f412ae1d06cc2a
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-08-16 13:42:00 +0000

    r5117@ferrari:  chisel | 2006-08-11 08:57:33 +0100  copied templates, etc from trunk/

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -4,33 +4,71 @@ use strict;
 use warnings;
 
 use Catalyst::Runtime '5.70';
+use Catalyst qw/
+    -Debug
+    Dumper
+    StackTrace
 
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a YAML file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root 
-#                 directory
+    ConfigLoader
 
-use Catalyst qw/-Debug ConfigLoader Static::Simple/;
+    Email
+    Static::Simple
 
-our $VERSION = '0.01';
+    Session
+    Session::Store::FastMmap
+    Session::State::Cookie
 
-# Configure the application. 
-#
-# Note that settings in Parley.yml (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with a external configuration file acting as an override for
-# local deployment.
+    Authentication
+    Authentication::Store::DBIC
+    Authentication::Credential::Password
 
-__PACKAGE__->config( name => 'Parley' );
+    Prototype
+    FillInForm
+    DefaultEnd
+/;
 
-# Start the application
+
+our $VERSION = '0.10-pre';
+
+__PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
 
+# only show certain log levels in output
+__PACKAGE__->log (Catalyst::Log->new( @{__PACKAGE__->config->{log_levels}} ));
+
+# I'm sure there's a (better) way to do this by overriding set()/get() in Class::Accessor
+{
+    sub set_get {
+        my $c = shift;
+        my $key = shift;
+
+        if(@_ == 1) {
+            $c->stash->{$key} = $_[0];
+        }
+        elsif(@_ > 1) {
+            $c->stash->{$key} = [@_];
+        }
+
+        $c->stash->{$key};
+    }
+
+    sub _authed_user {
+        my $c = shift;
+        $c->set_get('authed_user', @_);
+    }
+    sub _current_post {
+        my $c = shift;
+        $c->set_get('current_post', @_);
+    }
+    sub _current_thread {
+        my $c = shift;
+        $c->set_get('current_thread', @_);
+    }
+    sub _current_forum {
+        my $c = shift;
+        $c->set_get('current_forum', @_);
+    }
+}
 
 =head1 NAME
 
@@ -42,7 +80,40 @@ Parley - Catalyst based application
 
 =head1 DESCRIPTION
 
-[enter your description here]
+Catalyst driven forum application
+
+=head1 EVIL, LAZY STASH ACCESS
+
+I know someone will look at this at some point and tell me this is evil, but
+I've added some get/set method shortcuts for commonly used stash items.
+
+=over 4
+
+=item $c->_authed_user
+
+get/set value stored in $c->stash->{_authed_user}:
+
+  $c->_authed_user( $some_value );
+
+=item $c->_current_post
+
+get/set value stored in $c->stash->{_current_post}:
+
+  $c->current_post( $some_value );
+
+=item $c->_current_thread
+
+get/set value stored in $c->stash->{_current_thread}:
+
+  $c->_current_thread( $some_value );
+
+=item $c->_current_forum
+
+get/set value stored in $c->stash->{_current_forum}:
+
+  $c->_current_forum( $some_value );
+
+=back
 
 =head1 SEE ALSO
 
@@ -50,7 +121,7 @@ L<Parley::Controller::Root>, L<Catalyst>
 
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< <chisel@herlpacker.co.uk> >>
 
 =head1 LICENSE
 
--- lib/Parley/Controller/Root.pm
+++ lib/Parley/Controller/Root.pm
@@ -10,6 +10,151 @@ use base 'Catalyst::Controller';
 #
 __PACKAGE__->config->{namespace} = '';
 
+# pre-populate values in the stash if we're given "appropriate" information:
+# - _authed_user
+# - _current_post
+# - _current_thread
+# - _current_forum
+sub auto : Private {
+    my ($self, $c) = @_;
+
+    ############################################################
+    # if we have a user ... fetch some info (if we don't already have it)
+    ############################################################
+    if ( $c->user and not defined $c->authed_user ) {
+        $c->log->info('Fetching user information for ' . $c->user->id);
+
+        # get the person info for the username
+        my $row = $c->model('ParleyDB')->resultset('person')->find(
+            {
+                'authentication.username'   => $c->user->username(),
+            },
+            {
+                join => 'authentication',
+            },
+        );
+        $c->_authed_user( $row );
+
+        #####################################################################
+        # cater for database upgrades, and make sure the user has preferences
+        #####################################################################
+        #Parley::App::Helper->user_preference_check($c);
+    }
+
+
+    ##################################################
+    # do we have a post id in the URL?
+    ##################################################
+    if (defined $c->request->param('post')) {
+        # make sure the paramter looks sane
+        if (not $c->request->param('post') =~ m{\A\d+\z}) {
+            $c->stash->{error}{message} = 'non-integer post id passed: ['
+                . $c->request->param('post')
+                . ']';
+            return;
+        }
+
+        # get the matching post
+        $c->_current_post(
+            $c->model('ParleyDB')->resultset('Post')->find(
+                {
+                    post_id  => $c->request->param('post')
+                }
+            )
+        );
+
+        # set the current_thread from the current_post
+        $c->_current_thread(
+            $c->_current_post->thread()
+        );
+
+        # set the current_forum from the current thread
+        $c->_current_forum(
+            $c->_current_thread->forum()
+        );
+    }
+    ##################################################
+    # (elsif) do we have a thread id in the URL?
+    ##################################################
+    elsif (defined $c->request->param('thread')) {
+        # make sure the paramter looks sane
+        if (not $c->request->param('thread') =~ m{\A\d+\z}) {
+            $c->stash->{error}{message} = 'non-integer thread id passed: ['
+                . $c->request->param('thread')
+                . ']';
+            return;
+        }
+
+        # get the matching thread
+        $c->_current_thread(
+            $c->model('ParleyDB')->resultset('thread')->find(
+                thread_id  => $c->request->param('thread'),
+            )
+        );
+
+        # set the current_forum from the current thread
+        $c->_current_forum(
+            $c->_current_thread->forum()
+        );
+    }
+    ##################################################
+    # do we have a forum id in the URL?
+    ##################################################
+    elsif (defined $c->request->param('forum')) {
+        # make sure the paramter looks sane
+        if (not $c->request->param('forum') =~ m{\A\d+\z}) {
+            $c->stash->{error}{message} = 'non-integer forum id passed: ['
+                . $c->request->param('forum')
+                . ']';
+            return;
+        }
+
+        # get the matching forum
+        $c->_current_forum(
+            $c->model('ParleyDB')->table('forum')->find(
+                forum_id  => $c->request->param('forum'),
+            )
+        );
+    }
+
+    # let things continue ...
+    return 1;
+}
+
+# if someone hits the application index '/' then send them off to the default
+# action (defined in the app-config)
+sub index : Private {
+    my ( $self, $c ) = @_;
+    # redirect to the default action
+    $c->response->redirect( $c->uri_for($c->config->{default_uri}) );
+}
+
+# by default show a 404 for anything we don't know about
+sub default : Private {
+    my ( $self, $c ) = @_;
+
+    $c->response->status(404);
+    $c->response->body( '404 Not Found' );
+}
+
+
+# deal with the end of the phase
+sub end : ActionClass('RenderView') {
+    my ($self, $c) = @_;
+
+    # if we have any error(s) in the stash, automatically show the error page
+    if (defined $c->stash->{error}) {
+        $c->stash->{template} = 'error/simple';
+    }
+}
+
+
+1;
+
+__END__
+
+=pod
+
 =head1 NAME
 
 Parley::Controller::Root - Root Controller for Parley
@@ -20,30 +165,31 @@ Parley::Controller::Root - Root Controller for Parley
 
 =head1 METHODS
 
-=cut
+=head2 auto
+
+Used to fetch user-information if the user has authenticated.
+
+Also pre-populate current_(post|thread|forum) values in the stash if we have
+appropriate information in the URL's query parameters.
 
 =head2 default
 
-=cut
-
-sub default : Private {
-    my ( $self, $c ) = @_;
-
-    # Hello World
-    $c->response->body( $c->welcome_message );
-}
+Emit a 404 status and a 'Not Found' message.
 
 =head2 end
 
 Attempt to render a view, if needed.
 
-=cut 
+If I<error> is defined in the stash, render the error template.
 
-sub end : ActionClass('RenderView') {}
+=head2 index
+
+Redirect to the applications default action, as defined by I<default_uri> in
+parley.yml
 
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< <chisel@herlpacker.co.uk> >>
 
 =head1 LICENSE
 
@@ -52,4 +198,4 @@ it under the same terms as Perl itself.
 
 =cut
 
-1;
+vim: ts=8 sts=4 et sw=4 sr sta
