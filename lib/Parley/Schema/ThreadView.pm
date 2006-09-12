d033c40e5cbbf64467bf70eb7079f214f285d537
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-09-12 14:59:54 +0000

    r875@ferrari:  chisel | 2006-09-06 10:14:06 +0100  / reformatted some of the generated content in add_columns(...)

--- lib/Parley/Schema/ThreadView.pm
+++ lib/Parley/Schema/ThreadView.pm
@@ -10,38 +10,45 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components('ResultSetManager', "PK::Auto", "Core");
 __PACKAGE__->table("thread_view");
 __PACKAGE__->add_columns(
-  "watched",
-  {
+  "watched" => {
     data_type => "boolean",
     default_value => "false",
     is_nullable => 0,
     size => 1,
   },
-  "thread_view_id",
-  {
+
+  "thread_view_id" => {
     data_type => "integer",
     default_value => "nextval('thread_view_thread_view_id_seq'::regclass)",
     is_nullable => 0,
     size => 4,
   },
-  "last_notified",
-  {
+
+  "last_notified" => {
     data_type => "timestamp with time zone",
     default_value => undef,
     is_nullable => 1,
     size => 8,
   },
-  "thread",
-  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
-  "timestamp",
-  {
+
+  "thread" => {
+    data_type => "integer",
+    default_value => undef,
+    is_nullable => 0,
+    size => 4
+  },
+  "timestamp" => {
     data_type => "timestamp with time zone",
     default_value => "now()",
     is_nullable => 0,
     size => 8,
   },
-  "person",
-  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
+  "person" => {
+    data_type => "integer",
+    default_value => undef,
+    is_nullable => 0,
+    size => 4
+  },
 );
 __PACKAGE__->set_primary_key("thread_view_id");
 __PACKAGE__->add_unique_constraint("thread_view_person_key", ["person", "thread"]);
