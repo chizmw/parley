5f6d22fbe817b889751056bdf3876cd38093ace1
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2006-09-14 11:12:44 +0000

    r1222@ferrari:  chisel | 2006-09-14 09:04:21 +0100  / tweaked send_email() to use ->{template}{text}  + updated and extended POD

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -27,7 +27,7 @@ use Catalyst qw/
 /;
 
 
-our $VERSION = '0.10-pre';
+our $VERSION = '0.50-pre';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
@@ -115,6 +115,19 @@ sub login_if_required {
 sub send_email {
     my ($c, $options) = @_;
 
+    # preparing for future expansion, where we intend to build multipart emails
+    # and we'll be using ->{template}{text} and ->{template}{html}
+    if (            exists $options->{template}
+            and not exists $options->{template}{text}
+    ) {
+        $c->log->warn(
+              $options->{template}{text}
+            . q{: plain-text template name should be stored in }
+            . q{->{template}{text} instead of ->{template}}
+        );
+        $options->{template}{text} = $options->{template};
+    }
+
     # send an email off to the new user
     my $email_status = $c->email(
         header => [
@@ -125,7 +138,7 @@ sub send_email {
 
         body => $c->view('Plain')->render(
             $c,
-            $options->{template},
+            $options->{template}{text},
             {
                 additional_template_paths => [ $c->config->{root} . q{/email_templates}],
 
@@ -176,11 +189,34 @@ Catalyst driven forum application
 
 =head1 METHODS
 
-=head2 is_logged_in
+=head2 application_email_address($c)
+
+=over 4
+
+B<Return Value:> $string
+
+=back
+
+Returns the email address string for the application built from the
+I<from_name> and I<from_address> in the alerts section of parley.yml
+
+=head2 is_logged_in($c)
+
+=over 4
+
+B<Return Value:> 0 or 1
+
+=back
 
 Returns 1 or 0 depending on whether there is a logged-in user or not.
 
-=head2 login_if_required($message)
+=head2 login_if_required($c,$message)
+
+=over 4
+
+B<Return Value:> Void Context
+
+=back
 
 If a user isn't logged in, send them to the login page, optionally setting the
 message for the login box.
@@ -188,6 +224,42 @@ message for the login box.
 Once logged in the user should (by virtue of stored session data, and login
 magic) be redirected to wherever they were trying to view before the required
 login.
+
+=head2 send_email($c,\%options)
+
+=over 4
+
+B<Return Value:> 0 or 1
+
+=back
+
+Send an email using the render() method in the TT view. \%options should
+contain the following keys:
+
+=over 4
+
+=item headers
+
+Header fields to be passed though to the call to L<Catalyst::Plugin::Email>.
+
+=item person
+
+A Parley::Schema::Person object for the intended recipient of the message.
+
+Or, any object with an email() method, and methods to match
+"S<[% person.foo() %]>"
+methods called in the email template(s).
+
+=item template
+
+Used to store the name of the email template(s) to be sent. I<Currently the
+application only sends plain-text emails, so only one file is specified.>
+
+The text template name should be passed in ->{template}{text}.
+
+The html template name should be passed in ->{template}{html}. (I<Not Implemented>)
+
+=back
 
 =head1 EVIL, LAZY STASH ACCESS
 
@@ -224,7 +296,7 @@ get/set value stored in $c->stash->{_current_forum}:
 
 =head1 SEE ALSO
 
-L<Parley::Controller::Root>, L<Catalyst>
+L<Parley::Controller::Root>, L<Catalyst::Plugin::Email>, L<Catalyst>
 
 =head1 AUTHOR
 
