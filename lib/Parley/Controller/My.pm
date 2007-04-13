c4180374fad26bbc137140fa9b98cf527168c869
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-04-13 20:22:29 +0000

    r3138@wiggin:  chisel | 2007-04-13 19:19:30 +0100  Nothing special ... just make a start on prefs working again

--- lib/Parley/Controller/My.pm
+++ lib/Parley/Controller/My.pm
@@ -4,6 +4,53 @@ use strict;
 use warnings;
 use base 'Catalyst::Controller';
 
+sub auto : Private {
+    my ($self, $c) = @_;
+    # need to be logged in to perform any 'my' actions
+    my $status = $c->login_if_required(
+        q{You must be logged in before you can access this area}
+    );
+    if (not defined $status) {
+        return 0;
+    }
+
+    # undecided if you need to be authed to perform 'my' actions
+    #if (not Parley::App::Helper->is_authenticted($c)) {
+    #    $c->stash->{error}{message} = q{You need to authenticate your registration before you can start a new topic.};
+    #}
+
+    return 1;
+}
+
+
+sub index : Private {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Parley::Controller::My in My.');
+}
+
+
+sub preferences : Local {
+    my ($self, $c) = @_;
+    my ($tz_categories);
+
+    # where did we come from? it would be nice to return there when we're done
+    if ($c->request->referer() !~ m{/my/preferences}xms) {
+        $c->session->{my_pref_came_from} = $c->request->referer();
+    }
+
+    # what's the current time? then we can show it in the TZ area
+    $c->stash->{current_time} = DateTime->now();
+
+    # fetch timezone categories
+    $tz_categories = DateTime::TimeZone->categories();
+    $c->stash->{tz_categories} = $tz_categories;
+
+
+
+    return;
+}
+
 =head1 NAME
 
 Parley::Controller::My - Catalyst Controller
@@ -14,23 +61,11 @@ Catalyst Controller.
 
 =head1 METHODS
 
-=cut
-
-
 =head2 index 
-
-=cut
-
-sub index : Private {
-    my ( $self, $c ) = @_;
-
-    $c->response->body('Matched Parley::Controller::My in My.');
-}
-
 
 =head1 AUTHOR
 
-Chisel Wright,,,
+Chisel Wright C<< chiselwright@users.berlios.de> >>
 
 =head1 LICENSE
 
