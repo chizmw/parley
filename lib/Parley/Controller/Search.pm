8a447659f473c31db4331c08bcc1a92694945029
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-04-19 16:49:38 +0000

    r3174@wiggin:  chisel | 2007-04-18 18:56:12 +0100  + add basic search/forum URL and functionality

--- lib/Parley/Controller/Search.pm
+++ lib/Parley/Controller/Search.pm
@@ -10,6 +10,29 @@ sub index : Private {
     $c->response->body('Matched Parley::Controller::Search in Search.');
 }
 
+sub forum :Local {
+    my ($self, $c) = @_;
+    my ($search_terms, $resultset);
+
+    # the search terms
+    $search_terms = $c->request->param('search_terms');
+
+    # save the search terms for the template to display
+    $c->stash->{search_terms}{raw} = $search_terms;
+
+    # search for any posts in the forum with the search_terms (phrase) in the
+    # subject or body
+    $resultset = $c->model('ParleyDB')->resultset('Post')->search(
+        {
+            subject     => { 'ilike' => "%$search_terms%" },
+        }
+    );
+
+    if ($resultset->count() > 0) {
+        $c->stash->{search_results} = $resultset;
+    }
+}
+
 =head1 NAME
 
 Parley::Controller::Search - Catalyst Controller
