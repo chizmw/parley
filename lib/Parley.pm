b6e1f1dee34d8d8fa2bfa4e06b9942e48ae8cee7
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-05-04 21:10:00 +0000

    r3222@wiggin:  chisel | 2007-05-04 19:02:12 +0100  + final preparation for 0.52

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -28,7 +28,7 @@ use Catalyst qw/
 
 use Parley::App::Communication::Email qw( :email );
 
-our $VERSION = '0.52-pre';
+our $VERSION = '0.52';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
