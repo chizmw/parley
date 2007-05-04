d19bef5725995630f8a83ea27f7e6177cc15dff2
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-05-04 09:19:14 +0000

    r3197@wiggin:  chisel | 2007-05-04 08:27:00 +0100  Fixing version at 0.51 for tagging/release

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -28,7 +28,7 @@ use Catalyst qw/
 
 use Parley::App::Communication::Email qw( :email );
 
-our $VERSION = '0.51-pre';
+our $VERSION = '0.51';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
