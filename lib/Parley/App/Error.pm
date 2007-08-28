2465d21e0acb80502b1fccf1be1d627a65124893
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-08-28 19:30:34 +0000

    r3738@wiggin:  chisel | 2007-08-17 08:06:13 +0100  + start to factor out useful error methods into one place

---
+++ lib/Parley/App/Error.pm
@@ -0,0 +1,36 @@
+package Parley::App::Error;
+# vim: ts=8 sts=4 et sw=4 sr sta
+use strict;
+use warnings;
+
+use Perl6::Export::Attrs;
+
+sub has_errors :Export(:methods) {
+    my ($c) = @_;
+
+    return (exists $c->stash->{view}{error}{messages});
+}
+
+sub parley_warn :Export(:methods) {
+    my ($c, $error_msg) = @_;
+
+    # if we don't have an error "type" set it to warning
+    $c->stash->{view}{error}{type} ||= q{warning};
+
+    # push the incoming error onto the "error stack"
+    push @{ $c->stash->{view}{error}{messages} },
+        $error_msg;
+}
+
+sub parley_die {
+    my ($c, $error_msg) = @_;
+
+    # die overrides existing types
+    $c->stash->{view}{error}{type} = q{die};
+
+    # push the incoming error onto the "error stack"
+    push @{ $c->stash->{view}{error}{messages} },
+        $error_msg;
+}
+
+1;
