89c09bf3b9d56cedadbfcee1c5226cf6c0c43bad
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-08-31 19:20:19 +0000

    r3762@wiggin:  chisel | 2007-08-31 08:27:44 +0100  The start of 0.56 development; change the VERSION and add to to TODO list

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -28,7 +28,7 @@ use Catalyst qw/
 
 use Parley::App::Communication::Email qw( :email );
 
-our $VERSION = '0.55';
+our $VERSION = '0.56_1';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
