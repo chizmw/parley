ced20b666c93e22713f362efc8d6e4ac3e0c50d2
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-10-11 21:31:59 +0000

    r3938@wiggin:  chisel | 2007-10-11 08:56:58 +0100  + bumped version to _02  - removed stray test file from MANIFEST

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -28,7 +28,7 @@ use Catalyst qw/
 
 use Parley::App::Communication::Email qw( :email );
 
-our $VERSION = '0.57_01';
+our $VERSION = '0.57_02';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
