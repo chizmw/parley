e498b1b67f3686283c1791acc6a9622f5858ab0d
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-10-23 19:12:29 +0000

    r4022@wiggin:  chisel | 2007-10-23 19:13:02 +0100  + somewhere to keep track of contributors

---
+++ lib/Parley/Contributors.pm
@@ -0,0 +1,28 @@
+package Parley::Contributors;
+
+# this is a dummy package to provide POD
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Parley::Contributors - people that helped out along the way
+
+=head1 AUTHOR
+
+Chisel Wright C<< <chiselwright@users.berlios.de> >>
+
+=head1 PATCHES AND BUG-FIXES
+
+Kaare Rasmussen C<< CPANID: KAARE >>
+
+=head1 PROJECTS
+
+L<Catalyst>,
+L<DBIx::Class>
+
+=cut
