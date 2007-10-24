1863267675735af46768f04c6bfc6e04932e18cc
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-10-24 19:04:08 +0000

    r4047@wiggin:  chisel | 2007-10-24 19:28:26 +0100  + refactored common schema testing into a separate module  / modified authentication.t to test known columns

---
+++ t/schema/SchemaTest.pm
@@ -0,0 +1,170 @@
+package SchemaTest;
+# vim: ts=8 sts=4 et sw=4 sr sta
+use strict;
+use warnings;
+
+use Test::More;
+
+sub new {
+    my ($proto, $options) = @_;
+
+    my $self = (defined $options) ? $options : {};
+
+    bless $self, ref($proto) || $proto;
+
+    return $self;
+}
+
+sub methods {
+    my ($self, $hashref) = @_;
+
+    $self->{methods} = $hashref;
+
+    return;
+}
+
+sub run_tests {
+    my ($self) = @_;
+    my ($schema, $record, $resultset);
+
+    plan tests =>
+          7                             # fixed number of tests
+        + @{ $self->{methods}->{columns}   }
+        + @{ $self->{methods}->{relations} }
+        + @{ $self->{methods}->{custom}    }
+    ;
+
+    # make sure we can use the schema (namespace) module
+    use_ok( $self->{namespace} );
+
+    # get a schema to query
+    $schema = $self->{namespace}->connect(
+        $self->{dsn}
+    );
+    isa_ok($schema, $self->{namespace});
+
+    # get an object to test methods against
+    $record = $schema->resultset( $self->{moniker} )->search({})->first();
+    isa_ok($record, $self->{namespace} . '::' . $self->{moniker});
+
+    my @std_method_types = qw(columns relations custom);
+
+    foreach my $method_type (@std_method_types) {
+        SKIP: {
+            skip qq{no $method_type methods}, 1
+                unless @{ $self->{methods}->{$method_type} };
+
+            can_ok(
+                $record,
+                @{ $self->{methods}->{$method_type} },
+            );
+            # try calling each method
+            foreach my $method ( @{ $self->{methods}->{$method_type} } ) {
+                eval { $record->$method };
+                is($@, q{}, qq{calling $method didn't barf});
+            }
+        }
+    }
+
+    # resultset class methods - we need something slightly different here
+    SKIP: {
+        skip qq{no resultsets methods}, 1
+            unless @{ $self->{methods}->{resultsets} };
+
+        $resultset = $schema->resultset( $self->{moniker} )->search({});
+        can_ok(
+            $resultset,
+            @{ $self->{methods}->{resultsets} },
+        );
+    }
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+SchemaTest - a painless way for testing DBIC schema class methods
+
+=head1 DESCRIPTION
+
+It's really useful to be able to test and confirm that DBIC classes have and
+support a known set of methods.
+
+Testing these one-by-one is more than tedious and likely to discourage you
+from writing the relevant test scripts.
+
+As a lazy person myself I don't want to write numerous near-identical scripts.
+
+SchemaTest takes the copy-and-paste out of DBIC schema class testing.
+
+=head1 SYNOPSIS
+
+Create a test script that looks like this:
+
+    #!/usr/bin/perl
+    # vim: ts=8 sts=4 et sw=4 sr sta
+    use strict;
+    use warnings;
+
+    # load the module that provides all of the common test functionality
+    use FindBin qw($Bin);
+    use lib $Bin;
+    use SchemaTest;
+
+    my $schematest = SchemaTest->new(
+        {
+            dsn       => 'dbi:Pg:dbname=mydb',
+            namespace => 'MyDB::Schema',
+            moniker   => 'SomeTable',
+        }
+    );
+    $schematest->methods(
+        {
+            columns => [
+                qw[
+                    id
+                    column1
+                    column2
+                    columnX
+                    foo_id
+                ]
+            ],
+
+            relations => [
+                qw[
+                    foo
+                ]
+            ],
+
+            custom => [
+                qw[
+                    some_method
+                ]
+            ],
+
+            resultsets => [
+                qw[
+                ]
+            ],
+        }
+    );
+
+    $schematest->run_tests();
+
+Run the test script:
+
+  prove -l t/schematest/xx.mydb.t
+
+=head1 SEE ALSO
+
+L<DBIx::Class>
+
+=head1 AUTHOR
+
+Chisel Wright C<< <chiselwright@users.berlios.de> >>
+
+=cut
--- t/schema/authentication.t
+++ t/schema/authentication.t
@@ -3,18 +3,26 @@
 use strict;
 use warnings;
 
-# evil globals
-our ($schema, $methods, $namespace, $moniker);
+# load the module that provides all of the common test functionality
+use FindBin qw($Bin);
+use lib $Bin;
+use SchemaTest;
 
-BEGIN {
-    use Test::More;
-
-    $namespace = q{Parley::Schema};
-    $moniker   = q{Authentication};
-
-    $methods = {
+my $schematest = SchemaTest->new(
+    {
+        dsn       => 'dbi:Pg:dbname=parley',
+        namespace => 'Parley::Schema',
+        moniker   => 'Authentication',
+    }
+);
+$schematest->methods(
+    {
         columns => [
             qw[
+                authentication_id
+                username
+                password
+                authenticated
             ]
         ],
 
@@ -32,58 +40,7 @@ BEGIN {
             qw[
             ]
         ],
-    };
-
-    # the number of tests depends (partly) on the number of functions we have in $methods
-    plan tests =>
-          7                             # fixed number of tests
-        + @{ $methods->{columns} }
-        + @{ $methods->{relations} }
-        + @{ $methods->{custom} }
-    ;
-
-    use_ok( $namespace );
-
-    # get a schema to query
-    $schema = Parley::Schema->connect(
-        'dbi:Pg:dbname=parley'
-    );
-
-    isa_ok($schema, $namespace);
-}
-
-# get a product record
-my $product = $schema->resultset( $moniker )->search({})->first();
-isa_ok($product, $namespace . '::' . $moniker);
-
-# the standard test
-my @std_method_types = qw(columns relations custom);
-
-foreach my $method_type (@std_method_types) {
-    SKIP: {
-        skip qq{no $method_type methods}, 1
-            unless @{ $methods->{$method_type} };
-
-        can_ok(
-            $product,
-            @{ $methods->{$method_type} },
-        );
-        # try calling each method
-        foreach my $method ( @{ $methods->{$method_type} } ) {
-            eval { $product->$method };
-            is($@, q{}, qq{calling $method didn't barf});
-        }
     }
-}
+);
 
-# resultset class methods - we need something slightly different here
-SKIP: {
-    skip qq{no resultsets methods}, 1
-        unless @{ $methods->{resultsets} };
-
-    my $rs = $schema->resultset( $moniker )->search({});
-    can_ok(
-        $rs,
-        @{ $methods->{resultsets} },
-    );
-}
+$schematest->run_tests();
