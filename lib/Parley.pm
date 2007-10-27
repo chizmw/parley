848b8dfb9b672452143f92c6a4d6da7bb0a09a3f
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-10-27 09:47:47 +0000

    r4092@wiggin:  chisel | 2007-10-27 10:47:40 +0100  + bumped version  / updated manifest file

--- lib/Parley.pm
+++ lib/Parley.pm
@@ -29,7 +29,7 @@ use Catalyst qw/
 
 use Parley::App::Communication::Email qw( :email );
 
-our $VERSION = '0.57_07';
+our $VERSION = '0.57_08';
 
 __PACKAGE__->config( version => $VERSION );
 __PACKAGE__->setup;
