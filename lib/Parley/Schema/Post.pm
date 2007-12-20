63c3f79b75e0efffdedc45dd1013a7cbb2bafb54
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-12-20 10:15:48 +0000

    r4336@wiggin:  chisel | 2007-12-20 09:14:06 +0000  / moved :ResultSetManager classes into separate resultset class

--- lib/Parley/Schema/Post.pm
+++ lib/Parley/Schema/Post.pm
@@ -15,7 +15,7 @@ use Parley::App::DateTime qw( :interval );
 
 use base 'DBIx::Class';
 
-__PACKAGE__->load_components("ResultSetManager", "PK::Auto", "Core");
+__PACKAGE__->load_components("PK::Auto", "Core");
 __PACKAGE__->table("post");
 __PACKAGE__->add_columns(
   "id" => {
@@ -86,7 +86,10 @@ __PACKAGE__->add_columns(
     size => 8,
   },
 );
+
 __PACKAGE__->set_primary_key("id");
+__PACKAGE__->resultset_class('Parley::ResultSet::Post');
+
 __PACKAGE__->has_many(
     "threads" => "Thread",
     { "foreign.last_post_id" => "self.id" }
@@ -138,81 +141,6 @@ foreach my $datecol (qw/created edited/) {
 
 
 
-# we used to use ->slice() but it sopped working on page #2 (!!)
-# this may be slower [not benchmarked] but it works
-sub last_post_in_list : ResultSet {
-    my ($self, $post_list) = @_;
-    my ($current_post);
-
-    while (my $tmp = $post_list->next()) {
-        # do nothing, we're just iterating the list
-        $current_post = $tmp;
-        #warn qq{LOOP: } . ref($current_post);
-    }
-    # return the current post, which is the last one we saw
-    # i.e. the last one in the list
-    #warn qq{CURRENT: } . ref($current_post);
-    return $current_post;
-}
-
-
-sub next_post :ResultSet {
-    my ($self, $post) = @_;
-    my $next_post;
-
-    # we want to find the next post after the one we've been given, based on
-    # creation time
-    # if for some reason there are no matches, just return the post we were passed
-    $next_post = $self->search(
-        {
-            created    => { '>' => DateTime::Format::Pg->format_datetime($post->created()) },
-            thread_id  => $post->thread()->id(),
-        },
-        {
-            rows    => 1,
-        }
-    );
-
-    if (defined $next_post->first()) {
-        return $next_post->first();
-    }
-
-    return $post;
-}
-
-
-sub page_containing_post : ResultSet {
-    my ($self, $post, $posts_per_page) = @_;
-
-    my $position_in_thread = $self->thread_position($post);
-
-    # work out what page the Nth post is on
-    my $page_number = int(($position_in_thread - 1) / $posts_per_page) + 1;
-
-    return $page_number;
-}
-
-
-sub thread_position : ResultSet {
-    my ($self, $post) = @_;
-
-    if (not defined $post) {
-        warn('$post id undefined in call to Parley::Model::ParleyDB::Post->thread_position()');
-        return;
-    }
-
-    # explicitly 'deflate' the creation time, as DBIx::Class (<=v0.06003) doesn't deflate on search()
-    my $position = $self->count(
-        {
-            thread_id  => $post->thread()->id(),
-            created => {
-                '<='   => DateTime::Format::Pg->format_datetime($post->created())
-            },
-        }
-    );
-
-    return $position;
-}
 
 # accessor to use in search results to return context matches
 sub match_context {
