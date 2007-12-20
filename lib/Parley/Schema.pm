467ea2fd0136b16838b20b068f0e06faf4c34cce
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2007-12-20 10:15:41 +0000

    r4335@wiggin:  chisel | 2007-12-20 09:08:21 +0000  / explicitly load Parley::Schema modules by name

--- lib/Parley/Schema.pm
+++ lib/Parley/Schema.pm
@@ -1,13 +1,28 @@
 package Parley::Schema;
-
-# Created by DBIx::Class::Schema::Loader v0.03004 @ 2006-08-10 09:12:24
-
+# vim: ts=8 sts=4 et sw=4 sr sta
 use strict;
 use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_classes;
-#$ENV{DBIC_TRACE} = 1;
+# explicitly load Parley::Schema classes
+__PACKAGE__->load_classes(
+    [
+        'Authentication',
+        'EmailQueue',
+        'ForumModerator',
+        'Forum',
+        'PasswordReset',
+        'Person',
+        'Post',
+        'Preference',
+        'PreferenceTimeString',
+        'RegistrationAuthentication',
+        'TermsAgreed',
+        'Terms',
+        'Thread',
+        'ThreadView',
+    ]
+);
 
 1;
