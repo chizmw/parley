7bd4d48500dce140f13021b525acdff49a1d8b11
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2008-02-19 19:19:03 +0000

    r4681@wiggin:  chisel | 2008-02-19 19:16:05 +0000  + added a couple of overlooked resultset classes

---
+++ lib/Parley/ResultSet/Forum.pm
@@ -0,0 +1,42 @@
+package Parley::ResultSet::Forum;
+# vim: ts=8 sts=4 et sw=4 sr sta
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+sub available_list {
+    my ($resultsource) = @_;
+    my ($rs);
+
+    $rs = $resultsource->search(
+        {
+            active  => 1,
+        },
+        {
+            order_by    => 'name ASC',
+        }
+    );
+
+    return $rs;
+}
+
+sub record_from_id {
+    my ($resultsource, $forum_id) = @_;
+    my ($rs);
+
+    $rs = $resultsource->find(
+        {
+            'me.id'  => $forum_id,
+        },
+        {
+            prefetch => [
+                'last_post',
+            ],
+        }
+    );
+
+    return $rs;
+}
+
+1;
