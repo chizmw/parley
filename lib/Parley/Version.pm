fd8082db6bf75a82b75c2742a17e4689f3f59c3c
chiselwright <chiselwright@f74ee645-c3e3-0310-be06-c791151d01b6>
2008-10-21 22:41:13 +0000

    r8325@wiggin:  chisel | 2008-10-21 08:51:11 +0100  version bump

--- lib/Parley/Version.pm
+++ lib/Parley/Version.pm
@@ -5,7 +5,7 @@ use warnings;
 
 # from mst on #catalyst
 #our $VERSION = '1.000000'; $VERSION = eval $VERSION;
-use version; our $VERSION = qv(1.0.2)->numify;
+use version; our $VERSION = qv(1.0.3)->numify;
 
 
 package Parley::Version;
